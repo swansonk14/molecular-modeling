d2da892b84017e71d4f7cb5ce709639078da040c
Kyle Swanson <swansonk.14@gmail.com>
2017-07-14 23:55:42 -0700

    Shuffling

--- generator.py
+++ generator.py
@@ -4,6 +4,7 @@ import numpy as np
 class Generator():
 	def __init__(self, metadata, im_size):
 		self.metadata = metadata
+		np.random.shuffle(self.metadata)
 		self.im_size = im_size
 		self.im_shape = (im_size, im_size)
 		self.index = 0
@@ -17,8 +18,10 @@ class Generator():
 			batch = self.metadata[self.index:self.index + batch_size]
 			self.index += batch_size
 		else:
+			batch = self.metadata[self.index:]
 			diff = batch_size - (len(self.metadata) - self.index)
-			batch = self.metadata[self.index:] + self.metadata[:diff]
+			np.random.shuffle(self.metadata)
+			batch += self.metadata[:diff]
 			self.index = diff
 
 		# get the images and labels for the batch
@@ -42,10 +45,8 @@ class Generator():
 		if batch_size is None:
 			batch_size = len(self.metadata)
 
-		index = self.index
 		self.index = 0
+		np.random.shuffle(self.metadata)
 
 		while self.index + batch_size <= num_examples:
 			yield self.next(batch_size)
-
-		self.index = index
